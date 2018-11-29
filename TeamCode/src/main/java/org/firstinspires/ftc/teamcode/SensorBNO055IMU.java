1af4b1090a7d3de6071fceb6a622835eeeeb9bb6
Jackson Tint <geekpersonman@gmail.com>
2018-11-29 15:23:29 -0700

    Added sample IMU calibration to teamCode.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SensorBNO055IMU.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SensorBNO055IMU.java
@@ -31,7 +31,6 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
